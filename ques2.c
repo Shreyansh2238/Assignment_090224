d9878084d52444e119b8424fb59d750944c386a2
Shreyansh2238 <148929367+Shreyansh2238@users.noreply.github.com>
2024-03-14 18:44:49 +0530

    Update ques2.c

--- ques2.c
+++ ques2.c
@@ -1,14 +1,41 @@
-#include<stdio.h>
-void main()
-{
-    int num,div,i,c=0;
-    printf("Enter the dividend: ");
-    scanf("%d",&num);
-    printf("Enter the divisor: ");
-    scanf("%d",&div);
-    for(i=div;i<=num;i=i+div)
-    c++;
-    printf("Quotient= %d\n",c);
-    printf("Remainder: %d",(num-i+div));
+#include <stdio.h>
 
+void Elements(int arr[], int size) {
+    int sum = 0;
+    double average;
+    int count = 0;
+
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    average = (double)sum / size;
+
+    printf("Elements greater than the average (%.2f) are:\n", average);
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > average) {
+            printf("%d ", arr[i]);
+            count++;
+        }
+    }
+    if (count == 0) {
+        printf("None");
+    }
+    
+    printf("\n");
+}
+
+int main() {
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+    printf("Enter %d elements:\n", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    Elements(arr, size);
+
+    return 0;
 }
