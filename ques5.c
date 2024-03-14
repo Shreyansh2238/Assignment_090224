4a78eb4489d183ec60d8b12e201f43f222d35de9
Shreyansh2238 <148929367+Shreyansh2238@users.noreply.github.com>
2024-03-14 18:46:20 +0530

    Update ques5.c

--- ques5.c
+++ ques5.c
@@ -1,20 +1,47 @@
-#include<stdio.h>
-void main()
-{
-    int num,s=0;
-    char ch;
-    for(ch='Y';ch!='N'&&ch!='n';scanf(" %c",&ch))
-    {
-        if(ch=='Y'||ch=='y')
-        {
-        printf("Provide the number \n");
-        scanf("%d",&num);
-        if(num%2==0)
-        s=s+num;
+#include <stdio.h>
+
+void transpose(int mat[][3], int rows, int cols) {
+    int i, j;
+    for (i = 0; i < rows; i++) {
+        for (j = i + 1; j < cols; j++) {
+            int temp = mat[i][j];
+            mat[i][j] = mat[j][i];
+            mat[j][i] = temp;
         }
-        else
-        printf("Not a valid Input \n");
-        printf("Do you want to continue(Y/N): ");
     }
-    printf("The Sum is: %d",s);
+}
+
+void displayMatrix(int mat[][3], int rows, int cols) {
+    int i, j;
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int rows, cols;
+    printf("Enter the number of rows and columns of the matrix: ");
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[rows][cols];
+    int i, j;
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    
+    printf("\nOriginal Matrix:\n");
+    displayMatrix(matrix, rows, cols);
+
+    transpose(matrix, rows, cols);
+
+    printf("\nTranspose Matrix:\n");
+    displayMatrix(matrix, cols, rows);
+
+    return 0;
 }
