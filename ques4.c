988952ee409c17aa95765742fb68dfd7ef02a37a
Shreyansh2238 <148929367+Shreyansh2238@users.noreply.github.com>
2024-03-14 18:45:47 +0530

    Update ques4.c

--- ques4.c
+++ ques4.c
@@ -1,20 +1,28 @@
-#include<stdio.h>
-int main(){
-    int n;
-    printf("Enter size of traingle: ");
-    scanf("%d",&n);
+#include <stdio.h>
 
-    for (int i = 0; i <= n; i++)
-    {
-        for (int j = 0; j < n-i; j++)
-        {
-            printf(" ");
+// Function to check if arr2[] is a subset of arr1[]
+int isSubset(int arr1[], int arr2[], int m, int n) {
+    int i = 0, j = 0;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++) {
+            if(arr2[i] == arr1[j])
+                break;
         }
-        for (int j = 0; j < i; j++)
-        {
-            printf("* ");
-        }
-        printf("\n");
+        if (j == m)
+            return 0;
     }
+    return 1;
+}
+
+int main() {
+    int arr1[] = {1, 2, 3, 4, 5, 6};
+    int arr2[] = {4, 5, 6};
+    int m = sizeof(arr1)/sizeof(arr1[0]);
+    int n = sizeof(arr2)/sizeof(arr2[0]);
+
+    if (isSubset(arr1, arr2, m, n))
+        printf("arr2[] is subset of arr1[]\n");
+    else
+        printf("arr2[] is not a subset of arr1[]\n");
     return 0;
 }
